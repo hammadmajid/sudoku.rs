f1f95beeef2db65d58b6e5d01ed751bf7635780e
Hammad Majid <hm.hammadmajid@gmail.com>
2023-07-22 19:18:57 +0500

    add startup setup and spawn default 2d camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
