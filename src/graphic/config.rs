3da88e2920ea6b5dd65805c35faa4861815cea55
Hammad Majid <hammadmajid@proton.me>
2023-08-02 17:34:03 +0500

    allow dead code

--- src/graphic/config.rs
+++ src/graphic/config.rs
@@ -3,7 +3,9 @@ use bevy::prelude::*;
 // Colors
 pub const BACKGROUND_COLOR: Color = Color::rgb(0.3, 0.3, 0.3);
 
+#[allow(dead_code)]
 pub const FILLED_CELL_COLOR: Color = Color::rgb(2.0, 2.0, 3.0);
+#[allow(dead_code)]
 pub const FIXED_CELL_COLOR: Color = Color::rgb(2.0, 2.0, 3.0);
 
 // Sizes
@@ -18,7 +20,10 @@ pub const MAJOR_LINE_THICKNESS: f32 = 4.0;
 pub const GRID_CENTER_X: f32 = 0.0;
 pub const GRID_LEFT_EDGE: f32 = GRID_CENTER_X - 0.5 * GRID_SIZE;
 pub const GRID_CENTER_Y: f32 = 0.0;
+#[allow(dead_code)]
 pub const GRID_BOT_EDGE: f32 = GRID_CENTER_Y - 0.5 * GRID_SIZE;
 
+#[allow(dead_code)]
 pub const NUM_OFFSET_X: f32 = 0.0 * CELL_SIZE;
+#[allow(dead_code)]
 pub const NUM_OFFSET_Y: f32 = 0.03 * CELL_SIZE;
